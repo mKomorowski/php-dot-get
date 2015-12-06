2fb1ddc3f7de11596eaaaa6dffb339254e3d30c5
mKomorowski <mkomorowski@windowslive.com>
2015-12-06 15:54:04 +0000

    Add library

---
+++ src/php-dot-get/DotArray.php
@@ -0,0 +1,57 @@
+<?php namespace mKomorowski\Notation;
+
+/**
+ * Class DotArray
+ * @package mKomorowski\Notation
+ */
+class Dot
+{
+    const DELIMITER = '.';
+
+    /**
+     * @var null
+     */
+    protected $default = null;
+
+    /**
+     * @param mixed $default
+     */
+    public function setDefault($default)
+    {
+        $this->default = $default;
+    }
+
+    /**
+     * @param array $array
+     * @param string $keyPath
+     * @return array|null
+     */
+    public function get(array $array, $keyPath)
+    {
+        return $this->extractValue($array, $keyPath);
+    }
+
+    /**
+     * @param array $array
+     * @param string $keyPath
+     * @return array|null
+     */
+    protected function extractValue($array, $keyPath)
+    {
+        if(gettype($keyPath) !== 'string') {
+            return $this->default;
+        }
+
+        $keys = explode(self::DELIMITER, $keyPath);
+
+        foreach($keys as $key) {
+            if(isset($array[$key])) {
+                $array = $array[$key];
+            } else {
+                return $this->default;
+            }
+        }
+
+        return $array;
+    }
+}
